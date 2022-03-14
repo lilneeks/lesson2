996e32b0c23ad040af2497a54ed57190f8197865
lilneeks <nickodc96@gmail.com>
2022-03-14 13:12:10 -0700

    demo almost done.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__ menu')
 
-let company = "Aisea";
-console.log(company);
-let name = 'Mosh'
+// display mobile menu
 
-console.log(name);
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
